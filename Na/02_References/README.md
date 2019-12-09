1d05e98817e0d5017790c6253e6dd1e2a308e191
PabloPiaggi <ppiaggi@gmail.com>
2019-12-08 23:38:02 -0500

    New folder MultiTP

--- Na/02_References/README.md
+++ Na/02_References/README.md
@@ -1,3 +1,3 @@
 # 2. References
 
-Before computing the free energies of Na in the multithermal-multibaric example we calculate references in the isothermal-isobaric ensemble. This directory contains an example at 1 bar and 350 K of the calculation of the free energy difference between the liquid and the solid. The folders Pressure1bar and Pressure10000bar contain the results of the calculations.
+Before computing the free energies of Na in the multithermal-multibaric ensemble we calculate references in the isothermal-isobaric ensemble. This directory contains an example at 1 bar and 350 K of the calculation of the free energy difference between the liquid and the solid. The folders Pressure1bar and Pressure10000bar contain the results of the calculations.
---
+++ Na/03_MultithermalMultibaric/Reweight/script.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+temp=np.linspace(350,450,11) # K
+press=np.linspace(0,10000,11)  # bar
+
+print("# vim:ft=plumed")
+print("")
+print("energy: READ FILE=COLVARtrim VALUES=energy  IGNORE_TIME")
+print("b1: READ FILE=COLVARtrim VALUES=b1.bias  IGNORE_TIME")
+print("uwall: READ FILE=COLVARtrim VALUES=uwall.bias IGNORE_TIME")
+print("refcv: READ FILE=COLVARtrim VALUES=refcv.morethan  IGNORE_TIME")
+print("voltmp: READ FILE=COLVARtrim VALUES=vol  IGNORE_TIME")
+print("")
+print("vol: COMBINE ARG=voltmp PARAMETERS=10.0 PERIODIC=NO")
+print("renergy: COMBINE ARG=energy PARAMETERS=-25000 PERIODIC=NO")
+print("weights: REWEIGHT_BIAS TEMP=400. ARG=b1.bias,uwall.bias")
+print("")
+
+for i in range(temp.shape[0]):
+	for j in range(press.shape[0]):
+		name=str(int(temp[i])) + "-" + str(int(press[j]))
+		print("weights" + name  + ": REWEIGHT_TEMPERATURE_PRESSURE TEMP=400 PRESSURE=301.10704285 REWEIGHT_TEMP=" + str(temp[i]) + " REWEIGHT_PRESSURE=" + str(press[j]*0.06022140857) + " ARG=renergy,vol")
+		#print("weights" + name  + ": REWEIGHT_TEMP TEMP=375 REWEIGHT_TEMP=" + str(temp[i]) + " ARG=renergy")
+		print("HISTOGRAM ...")
+		print("  ARG=refcv.morethan")
+		print("  GRID_MIN=-0.5")
+		print("  GRID_MAX=250.5")
+		print("  GRID_BIN=1000")
+		print("  BANDWIDTH=1.")
+		print("  LOGWEIGHTS=weights,weights" + name)
+		print("  NORMALIZATION=true")
+		print("  LABEL=hh"+ name)
+		print("  CLEAR=10000")
+		print("... HISTOGRAM")
+		print("")
+		print("DUMPGRID GRID=hh" + name + " FILE=histo" + name + " STRIDE=10000 FMT=%16.10f")
+		print("ff" + name + ": CONVERT_TO_FES GRID=hh" + name + " TEMP=" + str(temp[i]) )
+		print("DUMPGRID GRID=ff" + name + " FILE=fes" + name + " STRIDE=10000 FMT=%16.10f")
+		print("")
+
+print("")
+
+print("PRINT ARG=* FILE=COLVAR STRIDE=1 FMT=%16.10f")
